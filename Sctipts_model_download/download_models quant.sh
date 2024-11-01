8940bc5f4d4ff12417a3cab02a73823630cc37be
Kostas Ordoumpozanis <ko@aiteam.gr>
2024-11-01 14:54:46 +0200

    quantization script correction

--- Sctipts_model_download/download_models quant.sh
+++ Sctipts_model_download/download_models quant.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Create the vae directory if it doesn't already exist
+mkdir -p vae
+
 # List of URLs to download
 urls=(
     "https://huggingface.co/sdyy/OmniGen_quantization2/resolve/main/.gitattributes"
@@ -10,11 +13,16 @@ urls=(
     "https://huggingface.co/sdyy/OmniGen_quantization2/resolve/main/special_tokens_map.json"
     "https://huggingface.co/sdyy/OmniGen_quantization2/resolve/main/tokenizer.json"
     "https://huggingface.co/sdyy/OmniGen_quantization2/resolve/main/tokenizer_config.json"
+    "https://huggingface.co/sdyy/OmniGen_quantization2/resolve/main/vae/config.json?download=true"
+    "https://huggingface.co/sdyy/OmniGen_quantization2/resolve/main/vae/diffusion_pytorch_model.safetensors?download=true"
 )
 
 # Loop through each URL and use wget to download
 for url in "${urls[@]}"; do
-    wget "$url"
+    # Check if the URL contains "vae" to decide the destination folder
+    if [[ "$url" == *"vae"* ]]; then
+        wget -P vae "$url"
+    else
+        wget "$url"
+    fi
 done
-
- 
